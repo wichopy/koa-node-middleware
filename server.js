c0fa581b3bc6a7c2ebc09172dc4cb05a55cf5b8b
Will C <w.chou06@gmail.com>
2017-03-25 06:14:10 -0400

    clean up middleware code

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 const koa = require('koa');
 const app = new koa();
-const http = require('http');
+const rp = require('request-promise');
 
-var rp = require('request-promise');
-
+//put my middleware function in the middle (haha) of three middleware functions. Will be called when you visit localhost:4000
 app.use(async(ctx, next) => {
-    // Step 1: Log start date
     const start = new Date;
     console.log(`Started at: ${start}`);
-
-    await next(); // Step 2:  Go to the next middleware, the execution of the next two lines is paused
-    // Step 7: Do stuff after body is set and Step 6 
+    await next();
     console.log('Back at top of middleware flow.')
+    ctx.body = {
+        lat: ctx.request.lat,
+        lon: ctx.request.lon,
+        weather_icon: ctx.request.weather_icon,
+        weather_temp: ctx.request.temp_in_C
+    }
     const ms = new Date - start;
     console.log(`Method: ${ctx.method}, URL: ${ctx.url} - Elapsed: ${ms}ms`);
 });
 
-// Step 3: Do stuff
-//grab ip location and weather info
 app.use(async(ctx, next) => {
     const ipInfo = await rp('http://ip-api.com/json')
     var parsedIpInfo = JSON.parse(ipInfo)
     var lat = parsedIpInfo.lat
     var lon = parsedIpInfo.lon
-    ctx.request.lat = lat
-    ctx.request.lon = lon
     const weatherInfo = await rp(`https://api.wunderground.com/api/825fab01f5420319/forecast/geolookup/conditions/q/${lat},${lon}.json`)
-    ctx.body = JSON.parse(weatherInfo);
+    const parsedWeatherInfo = JSON.parse(weatherInfo);
+    ctx.request.lat = lat;
+    ctx.request.lon = lon;
+    ctx.request.weather_icon = parsedWeatherInfo.forecast.txt_forecast.forecastday[0].icon
+    ctx.request.temp_in_C = parsedWeatherInfo.forecast.simpleforecast.forecastday[0].high.celsius
     console.log('found ip and set weather.')
-    await next(); // Step 4: Go to the next middleware, the execution of the next line is paused
-    // Step 6: Do stuff after body is set
+    await next();
     console.log('Bubbling up');
 });
 
 app.use(async(ctx) => {
-    // Step 5: Body is set, "bubble" up, since there are no more next() calls
     console.log('End of middleware chain');
 });
 
-app.listen(3000);
+const PORT = 4000;
+console.log(`Listening on port ${PORT}`);
+app.listen(PORT);
